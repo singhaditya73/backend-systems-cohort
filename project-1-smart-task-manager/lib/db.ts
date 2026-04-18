4ac178c4db4033bb48929d526b3f86be3008b1a4
singhaditya73 <adityasinghrajput755@gmail.com>
2026-04-19 03:08:52 +0530

    completed the db layer

---
+++ project-1-smart-task-manager/lib/db.ts
@@ -0,0 +1,22 @@
+import { cache } from "next/dist/server/use-cache/use-cache-wrapper";
+
+const mongoose from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL!;
+if (!MONGODB_URL) {
+  throw new Error("URL doesn't exists");
+}
+let cached = (global as any).mongoose || { conn: null, promise: null };
+export async function ConnectDB() {
+  if (!cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URL).then((mongoose: any) => mongoose);
+  }
+  cached.conn = await cached.promise;
+  (global as any).mongoose = cached
+
+
+
+  return cached.conn;
+}
