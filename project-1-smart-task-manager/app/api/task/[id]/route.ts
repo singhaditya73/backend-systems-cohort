693203c97e54d1b8d51ecc7868814d835f5aec6b
singhaditya73 <adityasinghrajput755@gmail.com>
2026-04-24 23:58:36 +0530

    completed the route for task updation

--- project-1-smart-task-manager/app/api/task/[id]/route.ts
+++ project-1-smart-task-manager/app/api/task/[id]/route.ts
@@ -14,3 +14,83 @@ function getUserIdFromRequest(req: Request) {
     return null;
   }
 }
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+
+  try {
+    await connectDB();
+    const userId = getUserIdFromRequest(req);
+    if (!userId) {
+      return Response.json({ messgae: "unauthorize access" },
+        { status: 401 })
+    }
+    return Response.json({ Task }, { status: 200 })
+
+  } catch {
+    return Response.json(
+          { message: "Internal server error" },
+          { status: 500 }
+        );
+  }
+}
+
+export async function PUT(req: Request,
+  { params }: { params: { id: string } }) {
+  try {
+    await connectDB();
+    const userId = getUserIdFromRequest(req);
+    if (!userId) {
+      return Response.json({ message: "unauthorize access" },
+        {status:401})
+    }
+    const body = await req.json();
+    const updatedTask = await findOneAndUpdate(
+      { _id: params.id, userId },
+      body: {
+        new: true
+      }
+    )
+    if (!updatedTask) {
+      return Response.json(
+        {message: "task not updatted" },
+        {status:401}
+      )
+    }
+    return Response.json({
+      message:"task updated"
+    }, {task:updatedTask})
+  }
+  catch {
+    return Response.json({message:"internal server error"}, {status:500})
+  }
+}
+export async function DELETE(req: Request,
+  { params }: { params: { id: string } }) {
+  try {
+    await connectDB();
+    const userId = getUserIdFromRequest(req);
+    if (!userId) {
+      return Response.json({ message: "unauthorize access" },
+        {status:401})
+    }
+    const deleteTask = await findOneAndDelete({
+          _id: params.id,
+          userId,
+        })if (!deletedTask) {
+              return Response.json({ message: "Task not found" }, { status: 404 });
+            }
+
+            return Response.json(
+              { message: "Task deleted" },
+              { status: 200 }
+            );
+
+  } catch {
+    return Response.json(
+          { message: "Internal server error" },
+          { status: 500 }
+        );
+  }
+  }
