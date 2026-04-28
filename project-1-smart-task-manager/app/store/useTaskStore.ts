63e7eeaf07d7a30c398eda477dc27a8d7855ced2
singhaditya73 <adityasinghrajput755@gmail.com>
2026-04-29 01:43:06 +0530

    bug fix

--- project-1-smart-task-manager/app/store/useTaskStore.ts
+++ project-1-smart-task-manager/app/store/useTaskStore.ts
@@ -12,11 +12,12 @@ type TaskState = {
   token: string | null;
   tasks: Task[];
   selectedTask: Task | null;
+
   setToken: (token: string | null) => void;
   setTasks: (tasks: Task[]) => void;
   addTask: (task: Task) => void;
-  setSelectedTask: (task: Task | null) => void;
   updateTask: (task: Task) => void;
+  setSelectedTask: (task: Task | null) => void;
   removeTask: (taskId: string) => void;
   clearAll: () => void;
 };
@@ -26,21 +27,46 @@ export const useTaskStore = create<TaskState>((set) => ({
   tasks: [],
   selectedTask: null,
 
-  setToken: (token) => set({ token }),
-  setTasks: (tasks) => set({ tasks }),
-  addTask: (task) => set((state) => ({ tasks: [task, ...state.tasks] })),
-  setSelectedTask: (task) => set({ selectedTask: task }),
+  setToken: (token) =>
+    set({
+      token,
+    }),
+
+  setTasks: (tasks) =>
+    set({
+      tasks,
+    }),
+
+  addTask: (task) =>
+    set((state) => ({
+      tasks: [task, ...state.tasks],
+    })),
+
+  setSelectedTask: (task) =>
+    set({
+      selectedTask: task,
+    }),
+
   updateTask: (task) =>
     set((state) => ({
       tasks: state.tasks.map((t) => (t._id === task._id ? task : t)),
+
       selectedTask:
         state.selectedTask?._id === task._id ? task : state.selectedTask,
     })),
+
   removeTask: (taskId) =>
     set((state) => ({
       tasks: state.tasks.filter((t) => t._id !== taskId),
+
       selectedTask:
         state.selectedTask?._id === taskId ? null : state.selectedTask,
     })),
-  clearAll: () => set({ token: null, tasks: [], selectedTask: null }),
+
+  clearAll: () =>
+    set({
+      token: null,
+      tasks: [],
+      selectedTask: null,
+    }),
 }));
